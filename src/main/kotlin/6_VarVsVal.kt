89b3a626b107f7423bb75ce2b642fe93dd733cb5
Nurul Akmal <nakmal618@gmail.com>
2022-03-10 18:19:22 +0700

    contoh program menggunakan vardanval variabel

---
+++ src/main/kotlin/6_VarVsVal.kt
@@ -0,0 +1,11 @@
+//var di Kotlin digunakan untuk mendeklarasikan variabel yang nilainya dapat berubah sepanjang program dieksekusi.
+//val digunakan untuk mendeklarasikan variabel konstan yang nilainya tidak akan berubah selama program dieksekusi.
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    val phi = 3.14
+}
+
