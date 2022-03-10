42acd4bc27f16462e41144490a5f25d72ad3377d
Nurul Akmal <nakmal618@gmail.com>
2022-03-10 18:27:25 +0700

    contoh program menggunakan floting-point

---
+++ src/main/kotlin/8_Floating-Point.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //variabel maxFloat yaitu hanya 6 digit (kelebihan digit akan dibulatkan)
+    val maxFloat: Float = 9.123456789f
+    //var maxDouble presisi digit mencapai 10 digit.
+    val maxDouble: Double = 9.123456789
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+}
