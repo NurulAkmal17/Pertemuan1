262e6a0e4778660d0647f1e47d66fe67d9159b00
Nurul Akmal <nakmal618@gmail.com>
2022-03-10 14:33:48 +0700

    contoh Pertemuan1

---
+++ src/main/kotlin/MainApp.kt
@@ -0,0 +1,6 @@
+//fun adalah sebuah keyword yang menandakan sebuah fungsi di kotlin
+//fun main() adalah fungsi main diaplikasi Kotlin dimana sebagai titik awal ketika program dijalankan.
+fun main(args: Array<String>) {
+    //fungsi dasar untuk mencetak string ke layar.
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+}
