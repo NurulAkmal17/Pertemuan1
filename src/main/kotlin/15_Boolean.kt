bd070cb3041653aea64b4bb49bfacd244ee6d0ae
Nurul Akmal <nakmal618@gmail.com>
2022-03-17 08:51:22 +0700

    contoh program menggunakan Boolean

--- src/main/kotlin/15_Boolean.kt
+++ src/main/kotlin/15_Boolean.kt
@@ -1,4 +1,4 @@
-//Tipe data Boolean memiliki nilai true atau false. Nilai 0 dan 1 tidak berlaku untuk menunjuk nilai pada sebuah variabel bertipe Boolean.
+//Tipe data Boolean memiliki nilai true atau false.Nilai 0 dan 1 tidak berlaku untuk menunjuk nilai pada sebuah variabel bertipe Boolean.
 fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
